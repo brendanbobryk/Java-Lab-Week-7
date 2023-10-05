129ae3d81145113f6ee82fde828ae52a6f0fd35a
Brendan Bobryk <babobryk@gmail.com>
2023-10-05 13:13:18 -0700

    Created variables and prompts

--- week7-booleanvariables/src/main/java/com/cis084javaprogramming/App.java
+++ week7-booleanvariables/src/main/java/com/cis084javaprogramming/App.java
@@ -10,6 +10,10 @@ package com.cis084javaprogramming;
 
 public class App {
     public static void main(String[] args) {
+        boolean teacher, isOver65, hasMedical, eligible;
 
+        System.out.println("Are you a teacher? (Y/N): ");
+        System.out.println("Are you over 65 years old? (Y/N): ");
+        System.out.println("Do you have a medical condition? (Y/N): ");
     }
 }
