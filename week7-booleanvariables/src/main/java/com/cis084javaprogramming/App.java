6d15cdfe4ff1c9b5c2e38d731207e285e1328c67
Brendan Bobryk <babobryk@gmail.com>
2023-10-05 13:08:16 -0700

    Added comments and prepared program for writing

--- week7-booleanvariables/src/main/java/com/cis084javaprogramming/App.java
+++ week7-booleanvariables/src/main/java/com/cis084javaprogramming/App.java
@@ -1,13 +1,15 @@
 package com.cis084javaprogramming;
 
 /**
- * Hello world!
- *
+ * Name: Brendan Bobryk
+ * Student ID #: 1036738
+ * Class: CIS 084 Java Programming
+ * Date: 10/10/23
+ * Program: Boolean Variables
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+
     }
 }
