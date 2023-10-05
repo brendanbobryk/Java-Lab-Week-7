115a71cbe12c2d570497119b125199f9d8bf4aa5
Brendan Bobryk <babobryk@gmail.com>
2023-10-05 13:26:22 -0700

    Created logic algorithm for input pathing

--- week7-booleanvariables/src/main/java/com/cis084javaprogramming/App.java
+++ week7-booleanvariables/src/main/java/com/cis084javaprogramming/App.java
@@ -1,5 +1,7 @@
 package com.cis084javaprogramming;
 
+import java.util.Scanner;
+
 /**
  * Name: Brendan Bobryk
  * Student ID #: 1036738
@@ -12,8 +14,27 @@ public class App {
     public static void main(String[] args) {
         boolean teacher, isOver65, hasMedical, eligible;
 
+        Scanner input = new Scanner(System.in);
         System.out.println("Are you a teacher? (Y/N): ");
-        System.out.println("Are you over 65 years old? (Y/N): ");
-        System.out.println("Do you have a medical condition? (Y/N): ");
+        String answer = input.nextLine();
+        if (answer.toLowerCase() == "y") {
+            teacher = true;
+            System.out.println("Are you over 65 years old? (Y/N): ");
+            answer = input.nextLine();
+            if (answer.toLowerCase() == "y") {
+                isOver65 = true;
+            }
+            System.out.println("Do you have a medical condition? (Y/N): ");
+            answer = input.nextLine();
+            if (answer.toLowerCase() == "y") {
+                hasMedical = true;
+            }
+        } else {
+            System.out.println("You are not eligable for a vaccination.");
+            input.close();
+            return;
+        }
+
+        input.close();
     }
 }
