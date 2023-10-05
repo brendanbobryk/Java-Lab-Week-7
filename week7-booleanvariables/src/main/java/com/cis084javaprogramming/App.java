1da42fe6edfd0223c1f475268e2c1e6ede647774
Brendan Bobryk <babobryk@gmail.com>
2023-10-05 13:04:00 -0700

    Created project base with Maven

---
+++ week7-booleanvariables/src/main/java/com/cis084javaprogramming/App.java
@@ -0,0 +1,13 @@
+package com.cis084javaprogramming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
